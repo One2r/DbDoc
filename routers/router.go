bd131cc355f1facb44e8f092c021546121382c21
One2r <601941036@qq.com>
2016-12-27 21:13:19 +0800

    添加路由

--- routers/router.go
+++ routers/router.go
@@ -9,4 +9,5 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/ajax/GetDbByGid", &controllers.AjaxController{}, "Get:GetDbByGid")
 	beego.Router("/ajax/AddGroup", &controllers.AjaxController{}, "Get:AddGroup")
+	beego.Router("/ajax/AddDbConn", &controllers.AjaxController{}, "Get:AddDbConn")
 }
